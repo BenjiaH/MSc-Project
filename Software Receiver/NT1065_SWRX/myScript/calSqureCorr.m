70b86aba905f5a70c699b58624abc7d53967133c
benjia <benjia.h@qq.com>
2023-08-22 00:08:35 +0100

    Update script

--- Software Receiver/NT1065_SWRX/myScript/calSqureCorr.m
+++ Software Receiver/NT1065_SWRX/myScript/calSqureCorr.m
@@ -10,25 +10,27 @@ fclose(fileID);
 
 IQ = [IQCell{:, 1}, IQCell{:, 2}];
 squareEnergy = (IQ(:, 1) .^ 2 + IQ(:, 2) .^ 2);
+n = ((squareEnergy));
+
 figure(1);
+plot(n);
+hold on;
+averageNoise = 1.7e6;
+note = sprintf("Average noise: %d", averageNoise);
+% yline(averageNoise, '-', note, LineWidth=1.5,FontSize=18);
+
+
+
+filename = 'Is_and_Qs_signal_bak/Is_and_Qs_signal_60ms.log';
+fileID = fopen(filename);
+IQCell = textscan(fileID,'%f %f','Delimiter',',');
+fclose(fileID);
+
+IQ = [IQCell{:, 1}, IQCell{:, 2}];
+squareEnergy = (IQ(:, 1) .^ 2 + IQ(:, 2) .^ 2);
+figure(2);
 plot((squareEnergy));
 hold on;
 averageNoise = 1.7e6;
 note = sprintf("Average noise: %d", averageNoise);
 yline(averageNoise, '-', note, LineWidth=1.5,FontSize=18);
-
-
-
-% filename = 'Is_and_Qs_signal_bak/Is_and_Qs_signal_60ms.log';
-% fileID = fopen(filename);
-% IQCell = textscan(fileID,'%f %f','Delimiter',',');
-% fclose(fileID);
-% 
-% IQ = [IQCell{:, 1}, IQCell{:, 2}];
-% squareEnergy = (IQ(:, 1) .^ 2 + IQ(:, 2) .^ 2);
-% figure(2);
-% plot((squareEnergy));
-% hold on;
-% averageNoise = 1.7e6;
-% note = sprintf("Average noise: %d", averageNoise);
-% yline(averageNoise, '-', note, LineWidth=1.5,FontSize=18);
